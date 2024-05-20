b093aec98bdf8b1604d333670af90f19ede657e0
Siliveri Amarnath <154747575+SilverStark18@users.noreply.github.com>
2024-05-20 10:45:19 +0000

    made some changes

--- amargptall.py
+++ amargptall.py
@@ -3,17 +3,10 @@ import os
 import json
 from streamlit_lottie import st_lottie
 import google.generativeai as genai
-from dotenv import load_dotenv
 from io import BytesIO
 import requests
-from PyPDF2 import PdfReader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from PyPDF2 import pdf
 from PIL import Image
-from langchain_community.vectorstores import FAISS
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.chains.question_answering import load_qa_chain
-from langchain.prompts import PromptTemplate
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
 
 
 
@@ -257,91 +250,46 @@ if selected == "IMAGE CHAT":
 if selected == "PDF CHAT":
     lottie_cpdf = load_lottiefiles(r"pdfparser.json")
     st_lottie(lottie_cpdf, loop=True,quality="high", speed=1.25, key=None, height=350)
+            ##gemini  response
+    def get_gemini_response(input):
+            model=genai.GenerativeModel('gemini-pro')
+            response=model.generate_content(input)
+            return response.text
 
-    def get_pdf_text(pdf_docs):
-     text=""
-     for pdf in pdf_docs:
-        pdf_reader= PdfReader(pdf)
-        for page in pdf_reader.pages:
-            text+= page.extract_text()
-     return  text
-
-
-
-    def get_text_chunks(text):
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
-        chunks = text_splitter.split_text(text)
-        return chunks
-
-
-    def get_vector_store(text_chunks):
-        embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
-        vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
-        vector_store.save_local("faiss_index")
-
-
-    def get_conversational_chain():
-
-        prompt_template = """
-        Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
+    def input_pdf_text(uploaded_file):
+            reader=pdf.PdfReader(uploaded_file)
+            text=""
+            for page in range(len(reader.pages)):
+                page=reader.pages[page]
+                text+=str(page.extract_text())
+            return text
+    input_prompt="""
+      Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
         provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
         Context:\n {context}?\n
         Question: \n{question}\n
 
         Answer:
+    
         """
+    st.title("Chat with YOUR PDFS")
 
-        model = ChatGoogleGenerativeAI(model="gemini-pro",temperature=0.3)
+    uploaded_file=st.file_uploader("Upload Your Resume",type="pdf",help="please upload the pdf")
+    st.session_state['pdf_srchistory'].append(("PDFS UPLOADED",uploaded_file))
+    user_question=st.text_area("Paste the job description 🎯🔍🌐")
+    st.session_state["pdf_history"].append(("YOU", user_question))
+    if st.button("submit"):
+            with st.spinner("Processing..."):
+                if uploaded_file is not None:
+                    text=input_pdf_text(uploaded_file)
+                    response=get_gemini_response(input_prompt)
+                    output_Text=response
+                    st.write(response)
+                    st.balloons()
+                    st.session_state["pdf_history"].append(("PDF_BOT", output_Text))
+    st.warning("clear the cache at the top left side by clicking --->  ⋮  ")
+       
 
-        prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
-        chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
-
-        return chain
-
-    def main1():
-        
-        st.header("Chat with PDF using Gemini💁")
-
-        user_question = st.chat_input("Ask a Question from the PDF Files")
-
-        if user_question:
-            user_input(user_question)
-            #st.session_state['pdf_history'].append(("YOU", user_question))
-
-        st.title("Menu:")
-        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
-        
-        if st.button("Submit & Process") :
-                    with st.spinner("Processing..."):
-                        raw_text = get_pdf_text(pdf_docs)
-                        text_chunks = get_text_chunks(raw_text)
-                        get_vector_store(text_chunks)
-                        st.success("Done")
-                        pdf_names = [pdf.name for pdf in pdf_docs]
-                        st.session_state["pdf_srchistory"].append(("PDFS UPLOADED", pdf_names))
-                        st.balloons()
-                        
-                        
-
-    def user_input(user_question):
-        embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
-        
-        new_db = FAISS.load_local("faiss_index", embeddings)
-        docs = new_db.similarity_search(user_question)
-
-        chain = get_conversational_chain()
-
-        
-        response1 = chain( {"input_documents":docs, "question": user_question} , return_only_outputs=True)
-        
-        print(response1)
-        st.write("Reply: ", response1["output_text"])
-        
-        output_Text = response1["output_text"]
-        st.session_state["pdf_history"].append(("YOU", user_question))
-        st.session_state["pdf_history"].append(("PDF_BOT", output_Text))
-    if __name__ == "__main__":
-        main1()
 
 
 if selected == 'CREDITS':
